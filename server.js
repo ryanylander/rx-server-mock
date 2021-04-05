0027fbdceb008ce935bb909462b3942f569f34b4
Robbins Cleozier <robbins.cleozier@medlypharmacy.com>
2021-04-05 13:50:14 -0400

    Removing import

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 var port = process.env.PORT || 3001;
 var app = express();
 
-const dbPath = '/home/ec2-user/express-service/public/db/';
-const pharmacies = JSON.parse(fs.readFileSync(`${dbPath}/pharmacies.json`));
+//const dbPath = '/home/ec2-user/express-service/public/db/';
+//const pharmacies = JSON.parse(fs.readFileSync(`${dbPath}/pharmacies.json`));
 
 app.get('/', function(req, res) {
   res.json("RX App Mock Server");
 });
 
-app.get('/pharmacies', function(req, res) {
-  res.json(pharmacies.pharmacies);
-});
+// app.get('/pharmacies', function(req, res) {
+//   res.json(pharmacies.pharmacies);
+// });
 
-app.get('/pharmacies/ba5629ef-0dfb-40b9-aa09-664a8e2cdfb5', function(req, res) {
-  res.json(pharmacies.pharmacies[0]);
-});
+// app.get('/pharmacies/ba5629ef-0dfb-40b9-aa09-664a8e2cdfb5', function(req, res) {
+//   res.json(pharmacies.pharmacies[0]);
+// });
 
 app.listen(port);
 module.exports = app;
