4f1b66fcc650ab9e5cdd5a24a4e4c318e25a5105
Robbins Cleozier <robbins.cleozier@medlypharmacy.com>
2021-04-05 11:35:11 -0400

    Routing back to original

--- server.js
+++ server.js
@@ -1,39 +1,18 @@
-const fs = require('fs');
-const lodash = require('lodash');
-const jsonServer = require('json-server');
-const http = require('http');
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
 
-const port = process.env.PORT || 3000;
-let endpoints = [],
-  obj = {};
-let dbPath = __dirname + '/public/db/';
-let files = fs.readdirSync(dbPath);
-
-files.forEach((file) => {
-  let filePath = fs.readFileSync(dbPath + file);
-  let jsonObject = JSON.parse(filePath);
-
-  Object.keys(jsonObject).forEach(function (idx) {
-    endpoints.push(idx);
+app.get('/', function(req, res) {
+  res.send({
+    "Output" : "Hello World!"
   });
-  lodash.extend(obj, jsonObject);
 });
 
-const objOrdered = {};
-Object.keys(obj)
-  .sort()
-  .forEach(function (key) {
-    objOrdered[key] = obj[key];
+app.post('/', function(req, res) {
+  res.send({
+    "Output" : "Hello World!"
   });
+});
 
-const router = jsonServer.router(objOrdered);
-
-// Express
-const app = jsonServer.create();
-const middlewares = jsonServer.defaults();
-app.use(jsonServer.bodyParser);
-app.use(middlewares);
-app.use(router);
-app.listen(port, '0.0.0.0');
-
+app.listen(port);
 module.exports = app;
